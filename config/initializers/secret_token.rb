33915d9f33669b88f43c698b44f27dd26b8f9761
ahitendra <arvapalli.hitendra@gmail.com>
2016-09-10 15:51:30 +0530

    initializing the repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shareflix::Application.config.secret_key_base = 'b10e0531ba07905e2ad3cc9cddf1e7ea5b2c8380179b58346429db5193e4749c4afc0e98f60a3dbaca910c0ac6aeeeca34a520cc021cf0942b0c5a3ce1adaede'
